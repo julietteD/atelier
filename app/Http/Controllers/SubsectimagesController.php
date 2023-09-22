402e02345018b189d496fe974eab7e7b9a8136b1
Delpech <juliette@MacBook-Pro-de-Delpech.local>
2023-09-22 15:38:27 +0200

    erreur image

--- app/Http/Controllers/SubsectimagesController.php
+++ app/Http/Controllers/SubsectimagesController.php
@@ -14,12 +14,10 @@ class SubsectimagesController extends Controller
    
     public function store()
     {  
-        dd('hello');
         $photo = Subsectimages::create(request()->validate([
             'image' => ['required', 'image', 'max:2500', 'dimensions:max_width=1000'],
             'subsections_id' => ['required', 'integer']
         ]));
-        dd($photo);
      
         $photo->update(['image' => request('image')->store('uploads', 'public')]);
         return back();
